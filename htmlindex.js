3d2e18b318f752e3b39163b7954138e1ffe34265
nikkisharma4 <70011149+nikkisharma4@users.noreply.github.com>
2021-03-28 00:04:34 +0530

    Add files via upload

---
+++ htmlindex.js
@@ -0,0 +1,13 @@
+function imageslider(){
+    document.getElementById("slider").src="image2.jpg";
+}
+setInterval(imageslider, 2000);
+
+function imageslide(){
+    document.getElementById("slider").src="image3.jpg";
+}
+setInterval(imageslide, 4000);
+function imgslide(){
+    document.getElementById("slider").src="image1.jpg";
+}
+setInterval(imgslide, 6000);
